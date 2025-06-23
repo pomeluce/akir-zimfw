bf0c60db73a810a2f9d59142844901bfb87b0478
Tso <62343478+pomeluce@users.noreply.github.com>
2025-06-23 18:00:52 +0800

    feat: updated cache dir

--- modules/azim/hooks.zsh
+++ modules/azim/hooks.zsh
@@ -9,7 +9,7 @@ autoload -U compaudit compinit zrecompile
 
 # 如果 cache 目录未设置, 则使用默认的 cache 目录
 if [[ -z "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="$AZIM_HOME/cache"
+  ZSH_CACHE_DIR="$HOME/.cache/az_cache"
 fi
 
 # 默认执行历史命令绑定
